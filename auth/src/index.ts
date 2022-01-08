179b18b167d28593c1a43cd1e01739120d9b9eed
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-08 10:06:58 +0600

    JWT Authentication

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
-import { signupRouter } from "./routes/signupRoute";
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all('*', async (req, res) => {
@@ -17,6 +34,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from "../errors/bad-request-error";
-import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 
 
@@ -18,13 +20,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -36,9 +33,20 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt
+    };
+
     res.status(201).send(user);
   }
 );
-
 
 export { router as signupRouter };
